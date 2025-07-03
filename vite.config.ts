2ac63acec35bf282e29e4b98d82770b193d64216
Andrew Chernoff <whitedrew538@gmail.com>
2025-07-03 18:25:57 +0500

    fix: Vite version conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vercel()
-  ],
-})
+  plugins: [react()],
+});
