a78501fe8b2b4e5713434779274adb79d3794de1
Andrew Chernoff <whitedrew538@gmail.com>
2025-07-03 18:06:36 +0500

    fix: types error for html2pdf.js

--- src/html2pdf.d.ts
+++ src/html2pdf.d.ts
@@ -1,18 +1,4 @@
-declare module 'html2pdf.js' {
-  interface Html2PdfOptions {
-    margin?: number | [number, number, number, number];
-    filename?: string;
-    image?: { type: string; quality: number };
-    html2canvas?: any;
-    jsPDF?: any;
-  }
-  
-  interface Html2Pdf {
-    from: (element: HTMLElement) => Html2Pdf;
-    set: (options: Html2PdfOptions) => Html2Pdf;
-    save: () => Promise<void>;
-  }
-  
-  const html2pdf: Html2Pdf;
+declare module 'html2pdf.js/dist/html2pdf.min' {
+  import html2pdf from 'html2pdf.js';
   export default html2pdf;
 }
